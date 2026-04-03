c06c2a046e5580899baf5b48be141ebccd378f21
Huỳnh Gia Kiệt <bebau5076@gmail.com>
2026-04-03 08:35:53 +0700

    Fix lỗi Namespace và hoàn thành logic Bài 4 (Tuple List)

--- Pages/Bai4.cshtml.cs
+++ Pages/Bai4.cshtml.cs
@@ -7,16 +7,26 @@ namespace DemoLayouts.Pages
     public class Bai4Model : PageModel
     {
         [BindProperty]
-        public string Ten { get; set; }
+        public string Ten { get; set; } = string.Empty;
 
         [BindProperty]
         public double Diem { get; set; }
 
-        public static List<(string Ten, double Diem)> DanhSach = new List<(string, double)>();
+        // Dòng 14: Cú pháp Tuple phải có ngoặc (string, double)
+        public static List<(string Ten, double Diem)> DanhSach = new List<(string Ten, double Diem)>();
+
+        public void OnGet()
+        {
+            // Để trống hoặc khởi tạo dữ liệu nếu muốn
+        }
 
         public void OnPost()
         {
-            DanhSach.Add((Ten, Diem));
+            if (!string.IsNullOrEmpty(Ten))
+            {
+                // Dòng 19: Phải có 2 lần ngoặc: Add( (Ten, Diem) )
+                DanhSach.Add((Ten, Diem));
+            }
         }
     }
 }
